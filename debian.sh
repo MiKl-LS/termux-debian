e03aa4d4c259a12e9c5c9a7e71cc4db94d542bca
MiKl-LS <m4y0lm1ch43l@gmail.com>
2020-04-20 14:34:30 +0800

    Modify Neo-Oli's start-ubuntu.sh script

--- debian.sh
+++ debian.sh
@@ -57,22 +57,32 @@ function install {
 	fi
 	echo "Writing launch script..."
 	cat > ${script} <<- EOF
-	#!/data/data/com.termux/files/usr/bin/bash
-	unset LD_PRELOAD
-	proot \
-		-0 \
-		--link2symlink \
-		-r ~/${folder} \
-		-b /dev/ \
-		-b /sys/ \
-		-b /proc/ \
-		-b /data/data/com.termux/files/home \
-		/usr/bin/env  \
-			-i \
-			HOME=/root \
-			TERM="\$TERM" \
-			PATH=/bin:/usr/bin:/sbin:/usr/sbin \
-			/bin/bash --login
+		cd \$(dirname \$0)
+		## unset LD_PRELOAD in case termux-exec is installed
+		unset LD_PRELOAD
+		command="proot"
+		command+=" --link2symlink"
+		command+=" -0"
+		command+=" -r $folder"
+		command+=" -b /dev"
+		command+=" -b /proc"
+		## uncomment the following line to have access to the home directory of termux
+		#command+=" -b /data/data/com.termux/files/home:/root"
+		## uncomment the following line to mount /sdcard directly to / 
+		#command+=" -b /sdcard"
+		command+=" -w /root"
+		command+=" /usr/bin/env -i"
+			command+=" HOME=/root"
+			command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin"
+			command+=" TERM=\$TERM"
+			command+=" LANG=C.UTF-8"
+			command+=" /bin/bash --login"
+		com="\$@"
+		if [ -z "\$1" ];then
+			exec \$command
+		else
+			\$command -c "\$com"
+		fi	
 EOF
 	echo "Making $script executable..."
 	chmod +x $script
